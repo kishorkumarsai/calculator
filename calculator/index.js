030d1bf937c72e93d82a1a0d2076e424f13601ef
kishorkumar <149250307+kishorkumarsai@users.noreply.github.com>
2023-12-28 10:19:32 +0530

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,15 @@
+function displayValue(val){
+    document.getElementById("display").value = document.getElementById("display").value+val
+}
+
+function clearDisplay(){
+   document.getElementById("display").value= ""
+}
+
+function btnAns(){
+    var userInput =document.getElementById("display").value;
+
+    var result = eval(userInput);
+
+    document.getElementById("display").value = result;
+}
